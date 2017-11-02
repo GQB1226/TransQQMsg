9f2f1e684dd2b0d34e4bf373240ad7b1a58c3d20
GQB1226 <gqb1226@gmail.com>
2017-11-02 14:01:07 +0800

    LinuxClient add log

--- TransQQMsgClient/LinuxClient.py
+++ TransQQMsgClient/LinuxClient.py
@@ -2,14 +2,28 @@
 __author__ = 'gqb1226'
 import os
 import socket
-s=socket.socket()
-port=12260
-s.bind(('',port))
-s.listen(5)
-while True:
-    conn,addr=s.accept()
+import logging
+import time
+
+def main():
+    logging.basicConfig(filename="./server.log", level=logging.WARNING)
+    s=socket.socket()
+    port=12260
+    s.bind(('',port))
+    s.listen(5)
     while True:
-        data=conn.recvfrom(1024)
-        if len(data[0])<=0:
-            break
-        os.system('zenity --notification --window-icon="info" --text="有QQ消息了" ')
+        conn,addr=s.accept()
+        logging.warning('%s %s connected'%(addr,time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())))
+        while True:
+            try:
+                data=conn.recvfrom(1024)
+            except:
+                logging.warning('%s %s lost connect' % (addr, time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())))
+                break;
+            if len(data[0])<=0:
+                logging.warning('%s %s lost connect' % (addr, time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())))
+                break
+            os.system('zenity --notification --window-icon="info" --text="有QQ消息了" ')
+
+if __name__=='__main__':
+    main()
