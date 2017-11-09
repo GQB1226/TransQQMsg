dc2543af2007974653d7c251f08cea714b3580d5
GQB1226 <gqb1226@gmail.com>
2017-11-09 09:07:48 +0800

    添加心跳包检测

--- TransQQMsgClient/LinuxClient.py
+++ TransQQMsgClient/LinuxClient.py
@@ -5,25 +5,33 @@ import socket
 import logging
 import time
 
+
 def main():
     logging.basicConfig(filename="./server.log", level=logging.WARNING)
-    s=socket.socket()
-    port=12260
-    s.bind(('',port))
+    s = socket.socket()
+    port = 12260
+    s.bind(('', port))
     s.listen(5)
     while True:
-        conn,addr=s.accept()
-        logging.warning('%s %s connected'%(addr,time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())))
+        conn, addr = s.accept()
+        logging.warning('%s %s connected' % (addr, time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())))
         while True:
             try:
-                data=conn.recvfrom(1024)
+                data = conn.recv(1024)
             except:
                 logging.warning('%s %s lost connect' % (addr, time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())))
-                break;
-            if len(data[0])<=0:
-                logging.warning('%s %s lost connect' % (addr, time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())))
                 break
+            if len(data) <= 0:
+                logging.error('%s %s lost connect' % (addr, time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())))
+                break
+            str = data.decode()
+            # 2017-11-09 GQB1226 add心跳包,格式 hb:客户端发送的随机散列值
+            if str.startswith(('hb:')):
+                logging.info("%s %s send heartbeat pack" % (addr, time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())))
+                conn.send(b"hb:" + str.split(":")[1].encode(encoding="utf-8"))
+                continue
             os.system('zenity --notification --window-icon="info" --text="有QQ消息了" ')
 
-if __name__=='__main__':
+
+if __name__ == '__main__':
     main()
